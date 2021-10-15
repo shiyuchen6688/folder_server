64f72c1e8a5a7cecbdac01079581fdd52b1defbd
shiyuc <shiyuc@students.cs.ubc.ca>
2021-10-15 14:50:17 -0700

    done react setup, basic folder and file component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name="Folder1">
+        <Folder name="Folder2">
+          <File name="File2" />
+          <File name="File3" />
+        </Folder>
+        <File name="File1" />
+      </Folder>
+      <Folder name="Folder3" />
+
     </div>
   );
 }
 
+const Folder = (props) => {
+  const { name, children } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const direction = isOpen ? "down" : "right";
+  const folderState = isOpen ? " open " : " ";
+
+
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div>
+      <span onClick={handleClick}>
+        <i className={`angle ${direction} icon`}></i>
+        <i className={`blue folder${folderState}icon`}></i>
+        {props.name}
+      </span>
+      <div style={{ margin: '17px' }}>
+        {isOpen ? props.children : null}
+      </div>
+    </div>
+  );
+}
+
+const File = (props) => {
+  return <h5>{props.name}</h5>
+}
 export default App;
